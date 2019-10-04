0da1e591bdfb2fcc1d550a989d6243ccc7a60ed4
hwing777 <hwing777@gmail.com>
2019-10-04 01:10:32 -0700

    Clean Calculator a bit

--- calculator/src/Components/Board.js
+++ calculator/src/Components/Board.js
@@ -17,57 +17,57 @@ function Rectagle(props) {
     );
 }
   
-class Board extends React.Component {
+class Grid extends React.Component {
 
-renderSquare(i) {
-    return (
-    <Square
-        value={i}
-        onClick={() => this.props.onClick(i)}
-    />
-    );
+    renderSquare(i) {
+        return (
+        <Square
+            value={i}
+            onClick={() => this.props.onClick(i)}
+        />
+        );
+    }
+
+    render() {
+        return (
+        <div>
+            <div className="row">
+                {this.renderSquare("±")}
+                {this.renderSquare("^")}
+                {this.renderSquare("C")}
+                {<Rectagle value={"del"} onClick={() => this.props.onClick("del")}/>}
+            </div>
+            <div className="row">
+                {this.renderSquare("7")}
+                {this.renderSquare("8")}
+                {this.renderSquare("9")}
+                {this.renderSquare("/")}
+                {this.renderSquare("√")}
+            </div>
+            <div className="row">
+                {this.renderSquare("4")}
+                {this.renderSquare("5")}
+                {this.renderSquare("6")}
+                {this.renderSquare("x")}
+                {this.renderSquare("%")}
+            </div>
+            <div className="row">
+                {this.renderSquare("1")}
+                {this.renderSquare("2")}
+                {this.renderSquare("3")}
+                {this.renderSquare("-")}
+                {this.renderSquare("π")}
+            </div>
+            <div className="row">
+                {this.renderSquare("0")}
+                {this.renderSquare(".")}
+                {this.renderSquare("=")}
+                {this.renderSquare("+")}
+                {this.renderSquare("!")}
+            </div>
+        </div>
+        );
+    }
 }
 
-render() {
-    return (
-    <div>
-        <div className="row">
-            {this.renderSquare("±")}
-            {this.renderSquare("^")}
-            {this.renderSquare("C")}
-            {<Rectagle value={"del"} onClick={() => this.props.onClick("del")}/>}
-        </div>
-        <div className="row">
-            {this.renderSquare("7")}
-            {this.renderSquare("8")}
-            {this.renderSquare("9")}
-            {this.renderSquare("/")}
-            {this.renderSquare("√")}
-        </div>
-        <div className="row">
-            {this.renderSquare("4")}
-            {this.renderSquare("5")}
-            {this.renderSquare("6")}
-            {this.renderSquare("x")}
-            {this.renderSquare("%")}
-        </div>
-        <div className="row">
-            {this.renderSquare("1")}
-            {this.renderSquare("2")}
-            {this.renderSquare("3")}
-            {this.renderSquare("-")}
-            {this.renderSquare("π")}
-        </div>
-        <div className="row">
-            {this.renderSquare("0")}
-            {this.renderSquare(".")}
-            {this.renderSquare("=")}
-            {this.renderSquare("+")}
-            {this.renderSquare("!")}
-        </div>
-    </div>
-    );
-}
-}
-
-export default Board;
+export default Grid;
--- calculator/src/Components/Calculator.js
+++ calculator/src/Components/Calculator.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Board from './Board.js';
+import Grid from './Board.js';
 import './Components.css';
 
 
@@ -24,6 +24,123 @@ class Calculator extends React.Component {
   
     renderScreen(i) {
       return (<Screen value={i}/>);
+    }
+
+    addNumbers(i, value, calculation, saved, replaceScreen){
+        if(!replaceScreen && value != "0" && i != "π"){
+            if (value.length < 9){
+                this.setState({
+                    value: value.concat(i),
+                    calculation: calculation,
+                    saved: saved,
+                    replaceScreen: replaceScreen
+                });
+            } 
+        }
+        else if(i == "π"){
+            this.setState({
+                value: "3.1415926",
+                calculation: calculation,
+                saved: saved,
+                replaceScreen: replaceScreen
+            });
+        }
+        else{
+            this.setState({
+                value: i,
+                calculation: calculation,
+                saved: saved,
+                replaceScreen: false
+            });
+        }
+    }
+
+    clear(){
+        this.setState({
+            value: 0,
+            calculation: "none",
+            saved: 0,
+            replaceScreen: true
+        });
+    }
+
+    changeSign(value, calculation, saved){
+        this.setState({
+            value: -1*value,
+            calculation: calculation,
+            saved: saved,
+            replaceScreen: false
+        });
+    }
+
+    delete(value, calculation, saved, replaceScreen){
+        if(value.length == 1){
+            this.setState({
+                value: "0",
+                calculation: calculation,
+                saved: saved,
+                replaceScreen: true
+            });
+        }
+        else{
+            this.setState({
+                value: value.substring(0,value.length-1),
+                calculation: calculation,
+                saved: saved,
+                replaceScreen: replaceScreen
+            });
+        }
+    }
+
+    sqrt(value){
+        var answer = Math.sqrt(parseFloat(value));
+        var strAnswer = answer.toString();
+        if (strAnswer.length > 8){
+            if (answer < 10){
+                answer = answer.toFixed(8);
+            }
+            else{
+                answer = answer.toExponential(4);
+            }
+        }
+
+        this.setState({
+            value: answer,
+            calculation: "none",
+            saved: 0,
+            replaceScreen: true
+        });
+    }
+
+    factorial(value){
+        var answer = parseInt(value);
+        for(var i=answer-1; i>0; i--){
+            answer = answer*i;
+        }
+        this.setState({
+            value: answer,
+            calculation: "none",
+            saved: 0,
+            replaceScreen: true
+        });
+    }
+
+    percent(value){
+        var answer = 0.01*(parseFloat(value));
+        this.setState({
+            value: answer,
+            calculation: "none",
+            saved: 0,
+            replaceScreen: true
+        });
+    }
+
+    saveOperator(i, value){
+        this.setState({
+            calculation: i,
+            saved: value,
+            replaceScreen: true
+        });
     }
 
     performCalculation(saved, calculation, value){
@@ -62,93 +179,10 @@ class Calculator extends React.Component {
         });
     }
 
-    addNumbers(i, value, calculation, saved, replaceScreen){
-        if(!replaceScreen && value != "0" && i != "π"){
-            if (value.length < 9){
-                this.setState({
-                    value: value.concat(i),
-                    calculation: calculation,
-                    saved: saved,
-                    replaceScreen: replaceScreen
-                });
-            } 
-        }
-        else if(i == "π"){
-            this.setState({
-                value: "3.1415926",
-                calculation: calculation,
-                saved: saved,
-                replaceScreen: replaceScreen
-            });
-        }
-        else{
-            this.setState({
-                value: i,
-                calculation: calculation,
-                saved: saved,
-                replaceScreen: false
-            });
-        }
-    }
-
-    delete(value, calculation, saved, replaceScreen){
-        if(value.length == 1){
-            this.setState({
-                value: "0",
-                calculation: calculation,
-                saved: saved,
-                replaceScreen: true
-            });
-        }
-        else{
-            this.setState({
-                value: value.substring(0,value.length-1),
-                calculation: calculation,
-                saved: saved,
-                replaceScreen: replaceScreen
-            });
-        }
-    }
-
-    factorial(value, calculation, saved, replaceScreen){
-        var answer = parseInt(value);
-        for(var i=answer-1; i>0; i--){
-            answer = answer*i;
-        }
+    equals(){
         this.setState({
-            value: answer,
             calculation: "none",
-            saved: 0,
-            replaceScreen: true
-        });
-    }
-
-    sqrt(value, calculation, saved, replaceScreen){
-        var answer = Math.sqrt(parseFloat(value));
-        var strAnswer = answer.toString();
-        if (strAnswer.length > 8){
-            if (answer < 10){
-                answer = answer.toFixed(8);
-            }
-            else{
-                answer = answer.toExponential(4);
-            }
-        }
-
-        this.setState({
-            value: answer,
-            calculation: "none",
-            saved: 0,
-            replaceScreen: true
-        });
-    }
-
-    percent(value, calculation, saved, replaceScreen){
-        var answer = 0.01*(parseFloat(value));
-        this.setState({
-            value: answer,
-            calculation: "none",
-            saved: 0,
+            saved: "0",
             replaceScreen: true
         });
     }
@@ -168,55 +202,25 @@ class Calculator extends React.Component {
         // if operator
         else {
             // clear
-            if(i == "C"){
-                this.setState({
-                    value: 0,
-                    calculation: "none",
-                    saved: 0,
-                    replaceScreen: true
-                });
-            }
+            if(i == "C"){ this.clear(); }
             // change signs
-            if(i == "±"){
-                this.setState({
-                    value: -1*value,
-                    calculation: calculation,
-                    saved: saved,
-                    replaceScreen: false
-                });
-            }
+            if(i == "±"){ this.changeSign(value, calculation, saved); }
             // delete
             else if(i == "del"){ this.delete(value, calculation, saved, replaceScreen); }
             // sqrt
-            else if(i == "√"){ this.sqrt(value, calculation, saved, replaceScreen); }
+            else if(i == "√"){ this.sqrt(value); }
             // factorial
-            else if(i == "!"){ this.factorial(value, calculation, saved, replaceScreen); }
+            else if(i == "!"){ this.factorial(value); }
             // percent
-            else if(i == "%"){ this.percent(value, calculation, saved, replaceScreen); }
+            else if(i == "%"){ this.percent(value); }
             // else perform operations
             else{
                 // if you have an operator, save and store prev value
-                if(operators.includes(i)){
-                    this.setState({
-                        calculation: i,
-                        saved: value,
-                        replaceScreen: true
-                    });
-                }
-
+                if(operators.includes(i)){ this.saveOperator(i, value); }
                 // perform calculation if any operator is pushed
-                if(operators.includes(calculation)){
-                    this.performCalculation(saved, calculation, value);
-                }
-
+                if(operators.includes(calculation)){ this.performCalculation(saved, calculation, value); }
                 // if equal sign, clear everything and reset
-                if(i == "="){
-                    this.setState({
-                        calculation: "none",
-                        saved: "0",
-                        replaceScreen: true
-                    });
-                }
+                if(i == "="){ this.equals(); }
             }
         }
     }
@@ -226,7 +230,7 @@ class Calculator extends React.Component {
         <div>
             <Screen value = {this.state.value} />
           <div className="board">
-            <Board  
+            <Grid  
               onClick={i => this.handleClick(i)}
             />
           </div>
